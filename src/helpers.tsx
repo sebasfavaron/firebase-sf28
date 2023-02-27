e68d8ec7324fbef57207a7da53799d4f6cc68b72
Sebastian Favaron <sebastianfavaron@gmail.com>
2023-02-27 10:46:51 -0300

    Removed use-persisted-state package Implemented localStorage myself

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,4 +1,4 @@
-import createPersistedState from "use-persisted-state";
+import { useEffect, useState } from "react";
 
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -10,5 +10,27 @@ export type Collection = { name: string; id: string };
 export type Collections = { collections: Collection[] };
 export type CustomCollections = { collectionName: string; tokens: Token[] }[];
 
-export const useCollectionsState =
-  createPersistedState<CustomCollections>("custom-collections");
+export function useCollectionsState(defaultValue?: CustomCollections) {
+  return useLocalStorage<CustomCollections>("custom-collections", defaultValue);
+}
+
+export function useLocalStorage<T>(name: string, defaultValue?: T) {
+  const [state, updateState] = useState<T | undefined>(() => {
+    if (typeof window === "undefined") return defaultValue;
+    let localState = localStorage.getItem(name);
+    if (localState) {
+      localState = JSON.parse(localState);
+      if (localState) {
+        return localState as T; //TODO: check
+      }
+    }
+    return defaultValue;
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined")
+      localStorage.setItem(name, JSON.stringify(state));
+  }, [state, name]);
+
+  return [state, updateState];
+}
